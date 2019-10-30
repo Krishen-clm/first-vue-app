d87c8eb926e2554753f4afd6bff7e61003dc49b9
Krishen-clm <krishen.clm@gmail.com>
2019-10-30 23:53:53 +0100

    second commit

--- first-app/src/components/HelloWorld.vue
+++ first-app/src/components/HelloWorld.vue
@@ -1,142 +1,150 @@
 <template>
-  <v-container>
-    <v-layout
-      text-center
-      wrap
-    >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
-
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
-
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-    </v-layout>
+  <v-container fluid>
+    <div v-show="start">
+    <v-text-field label="Identifiant" v-model="identifiant"></v-text-field>
+    <v-text-field label="Mot de passe" v-model="password"></v-text-field>
+    <v-btn v-on:click="sInscrire">Inscription</v-btn>
+    <v-btn v-on:click="seConnecter">Connexion</v-btn>
+    </div>
+    <div v-show="pendu">
+    <p v-show="indice" class="text-center">{{words[n].indice}}</p>
+    <v-row justify:space arround>
+      <v-col v-for="l in words[n].longueur" v-bind:key="l">
+        <v-card>
+          <p class="text-center">{{numero_champs_texte[l-1]}}</p>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-text-field @keyup.enter="conditionValidation" v-bind:background-color="couleur" label="Entrez votre réponse" v-model="lettre"></v-text-field>
+    <p>Entrez 1 seule charactère</p>
+    <p> Lettres déjà utilisées : {{lettreEntree}}</p>
+    <v-btn block rounded x-large v-on:click="conditionValidation" class="center">Valider</v-btn>
+    </div>
   </v-container>
 </template>
 
 <script>
 export default {
   data: () => ({
-    ecosystem: [
+    words: [
       {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader'
+        mot: 'nuage',
+        longueur: 5,
+        indice: 'Dans le ciel'
       },
       {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify'
+        mot: 'soleil',
+        longueur: 6,
+        indice: 'Très chaud'
       },
       {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify'
+        mot: 'ordinateur',
+        longueur: 10,
+        indice: "Utile à l'ESIEA"
       }
     ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com'
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com'
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify'
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs'
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify'
-      }
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer'
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined'
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-      }
+    n: 0,
+    l: 0,
+    e: 0,
+    start: true,
+    pendu: false,
+    identifiant: '',
+    password: '',
+    faute: '',
+    fauteQ: '',
+    lettre: '',
+    couleur: '',
+    error: '',
+    success: 0,
+    indice: false,
+    premiere_lettre: '',
+    deuxieme_lettre: '',
+    troisieme_lettre: '',
+    quatrieme_lettre: '',
+    cinquieme_lettre: '',
+    sixieme_lettre: '',
+    septieme_lettre: '',
+    huitieme_lettre: '',
+    neuvieme_lettre: '',
+    dixieme_lettre: '',
+    lettreEntree: '',
+    numero_champs_texte: [
+      '?',
+      '?',
+      '?',
+      '?',
+      '?',
+      '?',
+      '?',
+      '?',
+      '?',
+      '?'
     ]
-  })
+  }),
+  methods: {
+    mot_suivant () {
+      if (this.n === this.words.length - 1) {
+        this.n = 0
+       }else {
+        this.n = this.n + 1
+      }
+      console.log(this.numero_champs_texte)
+      for (var i = 0; i < this.numero_champs_texte.length; i++) {
+        this.numero_champs_texte[i] = ' '
+      }
+      console.log(this.numero_champs_texte)
+      console.log(this.n)
+    },
+
+    conditionValidation () {
+      this.lettreEntree = this.lettreEntree + this.lettre.toLowerCase() + ' '
+      if (this.lettre.length !== 1) {
+        console.log(this.numero_champs_texte[2])
+        this.couleur = 'red'
+      } else if (this.words[this.n].mot.indexOf(this.lettre.toLowerCase()) === -1) {
+        this.couleur = ''
+        this.faute++
+        this.fauteQ++
+        if (this.fauteQ === 3) {
+          this.indice = true
+        }
+      } else {
+        this.success++
+        for (var i = 0; i < this.words[this.n].mot.length; i++) {
+          if (this.words[this.n].mot[i] === this.lettre.toLowerCase()) {
+            this.numero_champs_texte[i] = this.lettre.toLowerCase()
+          }
+        }
+        if (this.success === this.words[this.n].mot.length) {
+          this.success = 0
+          this.fauteQ = 0
+          this.indice = false
+          for (var i = 0; i < this.words[this.n].mot.length; i++) {
+            this.numero_champs_texte[i] = '?'
+          }
+          this.lettreEntree = ''
+          this.n++
+        }
+      }
+      this.lettre = ''
+    },
+    async sInscrire () {
+      await this.axios.post('http://localhost:4000/api/addLog', {
+        login: this.identifiant,
+        password: this.password
+      })
+    },
+
+    async seConnecter () {
+      const rep = await this.axios.post('http://localhost:4000/api/login', {
+        user: this.identifiant,
+        password: this.password
+      })
+      if (rep.data.message === 'connected') {
+        this.pendu = true
+        this.start = false
+      }
+    }
+  }
 }
 </script>
